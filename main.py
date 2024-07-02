afb693345c52aecbe1781b02f495a1dd5da63bb1
murilo5341 <murilomartinscruz@hotmail.com>
2024-07-01 22:22:21 -0300

    coloquei o .vscode e o main

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from repositories.usuario_repo import UsuarioRepo
 from routes import main_routes
 from util.auth import checar_permissao, middleware_autenticacao
 from util.exceptions import configurar_excecoes
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
 
 UsuarioRepo.criar_tabela()
 app = FastAPI(dependencies=[Depends(checar_permissao)])
@@ -11,3 +15,14 @@ app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 app.middleware(middleware_type="http")(middleware_autenticacao)
 configurar_excecoes(app)
 app.include_router(main_routes.router)
+
+app = FastAPI()
+app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates/main\pages")
+
+@app.get("/")
+def get_root(request: Request):
+    view_model = {
+        "request": request
+    }
+    return templates.TemplateResponse("/index.html", view_model)

c1a80cffe0c6e4ea5d0ec76f50680f9d4e888391
Murilo Martins Cruz <murilomartinscruz@hotmail.com>
2024-07-04 08:12:07 -0300

    atualização rotas

---
+++ routes/moderador_routes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+
+from util.templates import obter_jinja_templates
+
+router = APIRouter(prefix="/moderador")
+templates = obter_jinja_templates("templates/moderador")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/index.html", {"request": request})
+

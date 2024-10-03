b20e7023f80e658536d4f5aa16aad6e21f07a3c9
PedroAugustoMorgadoVentura <seu_email@example.com>
2024-10-03 07:07:09 -0300

    post-entrar resolvido

--- repositories/usuario_repo.py
+++ repositories/usuario_repo.py
@@ -87,7 +87,7 @@ class UsuarioRepo:
             dados = cursor.execute(
                 SQL_CHECAR_CREDENCIAIS, (email,)).fetchone()
             if dados:
-                if conferir_senha(senha, dados[3]):
+                if conferir_senha(senha, dados[1]):#alterando dados[3] para dados [1]
                     return (dados[0], dados[1], dados[2])
             return None
     

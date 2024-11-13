51d803e070e4a0aa9c46f7c64a799251040bbdae
PedroAugustoMorgadoVentura <seu_email@example.com>
2024-11-13 11:54:15 -0300

    atualização projeto sql.py

--- sql/projeto_sql.py
+++ sql/projeto_sql.py
@@ -12,4 +12,65 @@ CREATE TABLE IF NOT EXISTS "projeto" (
 	"endereco" TEXT NOT NULL
 )
 """
+SQL_INSERIR_PROJETO = """
+    INSERT INTO projeto(
+        "nome do projeto", 
+        "qtd_voluntarios", 
+        "Estado", 
+        "Municipio", 
+        "Data_inicio", 
+        "Data_fim", 
+        "meta_doacao", 
+        "descricao", 
+        "endereco"
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+"""
+
+SQL_OBTER_PROJETO_POR_ID = """
+    SELECT 
+        id, 
+        "nome do projeto", 
+        "qtd_voluntarios", 
+        "Estado", 
+        "Municipio", 
+        "Data_inicio", 
+        "Data_fim", 
+        "meta_doacao", 
+        "descricao", 
+        "endereco"
+    FROM projeto
+    WHERE id=?
+"""
+
+SQL_OBTER_QUANTIDADE_PROJETOS = """
+    SELECT COUNT(*)
+    FROM projeto
+"""
+
+SQL_NOME_PROJETO_EXISTE = """
+    SELECT COUNT(*)
+    FROM projeto
+    WHERE "nome do projeto"=?
+"""
+
+SQL_ATUALIZAR_PROJETO = """
+    UPDATE projeto
+    SET 
+        "nome do projeto" = ?, 
+        "qtd_voluntarios" = ?, 
+        "Estado" = ?, 
+        "Municipio" = ?, 
+        "Data_inicio" = ?, 
+        "Data_fim" = ?, 
+        "meta_doacao" = ?, 
+        "descricao" = ?, 
+        "endereco" = ?
+    WHERE id = ?
+"""
+
+SQL_EXCLUIR_PROJETO = """
+    DELETE FROM projeto
+    WHERE id = ?
+"""
 
